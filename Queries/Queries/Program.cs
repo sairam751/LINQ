6bc1b130c23826521a02c43c8b3bdf5b96e7a419
Sai Ram T <sairam751348@gmail.com>
2022-11-06 16:11:42 +0530

    Different types of LINQ syntax(Extension methods and Linq syntax)

--- Queries/Queries/Program.cs
+++ Queries/Queries/Program.cs
@@ -1,10 +1,34 @@
 ﻿
+using System;
+using System.Linq;
+
+
 namespace Queries
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var context = new PlutoContext();
+            //LINQ SYNTAX
+            //To get all c# cources
+            var query =
+                from c in context.Courses
+                where c.Name.Contains("c#")
+                orderby c.Name
+                select c;
+
+            foreach (var cours in query)
+                Console.WriteLine(cours.Name);
+
+            //Extension Methods
+            var course = context.Courses.
+                    Where(c => c.Name.Contains("c#"))
+                    .OrderBy(c => c.Name);
+            foreach (var c1 in course)
+                Console.WriteLine(c1.Name);
+            Console.ReadLine();
         }
+        
     }
 }
