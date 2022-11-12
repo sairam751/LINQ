f01b39d92e3da50f268ecf89c64ea8d97a3dfbdc
Sai Ram T <sairam751348@gmail.com>
2022-11-12 12:25:30 +0530

    DefferedExecution and intermediate execution

--- Queries/Queries/Program.cs
+++ Queries/Queries/Program.cs
@@ -1,10 +1,27 @@
 ﻿
+using System.Linq;
+
 namespace Queries
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var context = new PlutoContext();
+/*
+            //this can be seeen in SQL server instance
+
+            //Deffered Exceution
+            //this query will not execute immediately
+            var courses = context.Courses.Where(c => c.Level == 1);
+
+
+            //Intermediate Execution
+             //this query will  execute immediately
+            var courses1 = context.Courses.Where(c => c.Level == 1).ToList();
+*/
+
+             
         }
     }
 }
